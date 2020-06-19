b3965198d1f1c7736a9d8d15ff0c25f9fc2afb89
dharinisp <dharinisingh04@gmail.com>
2020-06-19 23:19:21 +0530

    pana

--- app/src/main/java/in/games/nidhimatka/Fragments/HowToPLayFragment.java
+++ app/src/main/java/in/games/nidhimatka/Fragments/HowToPLayFragment.java
@@ -1,5 +1,7 @@
 package in.games.nidhimatka.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 import in.games.nidhimatka.Activity.MainActivity;
+import in.games.nidhimatka.AppController;
+import in.games.nidhimatka.Config.URLs;
 import in.games.nidhimatka.R;
+import in.games.nidhimatka.Util.CustomVolleyJsonArrayRequest;
+import in.games.nidhimatka.Util.LoadingBar;
 
 public class HowToPLayFragment extends Fragment {
-
+    TextView bt_back,txtData,txtLink;
+    RelativeLayout rel_click;
+    LoadingBar progressDialog;
     public HowToPLayFragment() {
         // Required empty public constructor
     }
@@ -23,6 +43,66 @@ public class HowToPLayFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ((MainActivity) getActivity()).setTitle("How to Play");
-        return inflater.inflate(R.layout.fragment_how_to_p_lay, container, false);
+       View view =  inflater.inflate(R.layout.fragment_how_to_p_lay, container, false);
+        progressDialog=new LoadingBar(getActivity());
+
+        txtData=(TextView)view.findViewById(R.id.w2);
+        txtLink=(TextView)view.findViewById(R.id.link);
+        rel_click=(RelativeLayout)view.findViewById(R.id.rel_click);
+
+        rel_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String h= txtLink.getText().toString().trim();
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(h));
+                startActivity(intent);
+            }
+        });
+
+
+
+        getHowToPlayData();
+
+
+
+        return  view ;
+    }
+    private void getHowToPlayData() {
+        progressDialog.show();
+        String json_request_tag="json_how_request";
+        HashMap<String,String> params=new HashMap<String,String>();
+
+        CustomVolleyJsonArrayRequest customVolleyJsonArrayRequest=new CustomVolleyJsonArrayRequest(Request.Method.GET, URLs.URL_PLAY, params, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                try
+                {
+                    JSONObject jsonObject=response.getJSONObject(0);
+                    String data=jsonObject.getString("data");
+                    String link=jsonObject.getString("link");
+                    txtData.setText(data);
+                    txtLink.setText(String.valueOf(link));
+                    progressDialog.dismiss();
+                }
+                catch (Exception ex)
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(),""+ex.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(),""+error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(customVolleyJsonArrayRequest,json_request_tag);
+
+
     }
 }
